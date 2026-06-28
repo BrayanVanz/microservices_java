1f4ead9bc2ff08ec59ca4accf0cc6579d4cadc0a
Rafael Noll da Silva <rafael.silva@pubgenius.io>
2026-06-28 16:23:08 -0300

    Adicionando description

--- product-service/src/main/java/br/edu/atitus/productservice/dtos/ProductInDTO.java
+++ product-service/src/main/java/br/edu/atitus/productservice/dtos/ProductInDTO.java
@@ -3,12 +3,13 @@ package br.edu.atitus.productservice.dtos;
 import java.time.LocalDate;
 
 public record ProductInDTO(
-    String title,
-    String artist,
-    LocalDate releaseDate,
-    String genre,
-    String category,
-    String currency,
-    Double price,
-    String imageURL) {
+        String title,
+        String artist,
+        LocalDate releaseDate,
+        String genre,
+        String category,
+        String currency,
+        Double price,
+        String imageURL,
+        String description) {
 }
