e5b02dc7ecac742320cdd67c8a5691c40eeeb656
Rafael Noll da Silva <rafael.silva@pubgenius.io>
2026-06-28 15:19:02 -0300

    Novas imagens para itens de fallback

---
+++ product-service/src/main/resources/db_migration/V3__UpdateTableTbProductImages.sql
@@ -0,0 +1,23 @@
+UPDATE tb_product SET image_url = 'https://www.covercentury.com/covers/audio/t/The%20Beatles%20-%20Abbey%20Road%20(1969)%20[vk].jpg' WHERE title = 'Abbey Road' AND artist = 'The Beatles';
+
+UPDATE tb_product SET image_url = 'https://www.covercentury.com/covers/audio/t/thriller_michael-jackson_31619945.jpg' WHERE title = 'Thriller' AND artist = 'Michael Jackson';
+
+UPDATE tb_product SET image_url = 'https://i.scdn.co/image/ab67616d0000b2733ad5858333ae0ddaae61e5df' WHERE title = 'Back in Black' AND artist = 'AC/DC';
+
+UPDATE tb_product SET image_url = 'https://i.scdn.co/image/ab67616d0000b2739b9b36b0e22870b9f542d937' WHERE title = 'Random Access Memories' AND artist = 'Daft Punk';
+
+UPDATE tb_product SET image_url = 'https://i.scdn.co/image/ab67616d0000b273387a29c90de3b2398c29c34f' WHERE title = 'Kind of Blue' AND artist = 'Miles Davis';
+
+UPDATE tb_product SET image_url = 'https://i.scdn.co/image/ab67616d0000b27357df7ce0eac715cf70e519a7' WHERE title = 'Rumours' AND artist = 'Fleetwood Mac';
+
+UPDATE tb_product SET image_url = 'https://devinylhunter-records.de/cdn/shop/files/s-l1600-14_479cd73e-e6a5-46f2-bdb0-abe391f5e7b6.jpg?v=1693247475' WHERE title = 'Lemonade' AND artist = 'Beyoncé';
+
+UPDATE tb_product SET image_url = 'https://i.scdn.co/image/ab67616d0000b273db216ca805faf5fe35df4ee6' WHERE title = 'The Dark Side of the Moon' AND artist = 'Pink Floyd';
+
+UPDATE tb_product SET image_url = 'https://i.scdn.co/image/ab67616d0000b273c5649add07ed3720be9d5526' WHERE title = 'Blonde' AND artist = 'Frank Ocean';
+
+UPDATE tb_product SET image_url = 'https://i.scdn.co/image/ab67616d0000b2732b0fb8c4fb0d5eec84ca803b' WHERE title = 'Nevermind' AND artist = 'Nirvana';
+
+UPDATE tb_product SET image_url = 'https://m.media-amazon.com/images/I/81e-3lhg4rL.jpg' WHERE title = 'good kid, m.A.A.d city' AND artist = 'Kendrick Lamar';
+
+UPDATE tb_product SET image_url = 'https://i.scdn.co/image/ab67616d0000b273c88bae7846e62a8ba59ee0bd' WHERE title = 'Future Nostalgia' AND artist = 'Dua Lipa';
